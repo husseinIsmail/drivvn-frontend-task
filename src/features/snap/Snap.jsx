ebc070001752b948ac8b77e292213bd12fcecfd5
husseinismail <hismail.dev@gmail.com>
2023-03-15 19:14:00 +0000

    Add snap matching functionality

--- src/features/snap/Snap.jsx
+++ src/features/snap/Snap.jsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useLayoutEffect, useState } from 'react';
 import Card from '../../components/card/Card';
+import { debounce } from 'lodash';
 import './Snap.css';
 
 const Snap = () => {
@@ -7,6 +8,10 @@ const Snap = () => {
   const [cards, setCards] = useState([]);;
   const [currentCard, setCurrentCard] = useState(null);
   const [previousCard, setPreviousCard] = useState(null);
+  const [matchStatus, setMatchStatus] = useState(null);
+  const [snapSuitCount, setSnapSuitCount] = useState(0);
+  const [snapValueCount, setSnapValueCount] = useState(0);
+  const [allCardsWithdrawn, setAllCardsWithdrawn] = useState(false);
 
   useEffect(() => {
     const fetchDeck = async () => {
@@ -18,17 +23,51 @@ const Snap = () => {
     fetchDeck();
   }, []);
 
-  const fetchCard = async () => {
+  useLayoutEffect(() => {
+    const checkMatchingCards = () => {
+      console.log('here', currentCard, previousCard);
+      if (currentCard.suit === previousCard.suit) {
+        setSnapSuitCount(snapSuitCount => snapSuitCount + 1);
+        setMatchStatus('SNAP SUIT!');
+      } else if (currentCard.value === previousCard.value) {
+        setSnapValueCount(snapValueCount => snapValueCount + 1);
+        setMatchStatus('SNAP VALUE!');
+      } else {
+        setMatchStatus('');
+      }
+    }
+
+    if (currentCard && previousCard) {
+      checkMatchingCards();
+    }
+
+  }, [currentCard, previousCard]);
+
+
+  const drawCard = async () => {
+    const fetchedDraw = await fetchDraw();
+    const fetchedCard = fetchedDraw.cards[0];
+    const isLastCard = fetchedDraw.remaining === 0;
+
+    if (isLastCard) setAllCardsWithdrawn(true);
+    setCurrentCard(fetchedCard);
+    setPreviousCard(cards[cards.length - 1]);
+    setCards([...cards, fetchedCard]);
+  };
+
+  const fetchDraw = async () => {
     const drawResponse = await fetch(`https://deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=1`);
     const fetchedDraw = await drawResponse.json();
-    setCurrentCard(fetchedDraw.cards[0]);
-    setPreviousCard(cards[cards.length - 1]);
-    setCards([fetchedDraw.cards[0]]);
+    return fetchedDraw;
   };
 
   return (
     <div className='snap-container'>
-      <div className='card-container'>
+      <div className='snap-status'>
+        <h2>{matchStatus}</h2>
+      </div>
+
+      <div className='cards-container'>
         <div className='card-frame'>
           {previousCard && <Card image={previousCard.image} code={previousCard.code} />}
         </div>
@@ -36,9 +75,17 @@ const Snap = () => {
           {currentCard && <Card image={currentCard.image} code={currentCard.code} />}
         </div>
       </div>
-      <div className='draw-btn'>
-        <button onClick={fetchCard}>Draw card</button>
-      </div>
+
+      {allCardsWithdrawn ? (
+        <div className='snap-count'>
+          <h2>VALUE MATCHES: {snapValueCount}</h2>
+          <h2>SUIT MATCHES: {snapSuitCount}</h2>
+        </div>
+      ) : (
+        <div className='draw-btn'>
+          <button onClick={debounce(drawCard, 500)}>Draw card</button>
+        </div>
+      )}
     </div>
   );
 };
