e9420cb75b021523280999a95f219a68d0f1b282
husseinismail <hismail.dev@gmail.com>
2023-03-16 15:14:56 +0000

    Refactor fetch card

--- src/features/snap/Snap.jsx
+++ src/features/snap/Snap.jsx
@@ -1,6 +1,5 @@
-import { useEffect, useLayoutEffect, useState } from 'react';
+import { useEffect, useState, useLayoutEffect } from 'react';
 import Card from '../../components/card/Card';
-import { debounce } from 'lodash';
 import './Snap.css';
 
 const Snap = () => {
@@ -8,17 +7,16 @@ const Snap = () => {
   const [cards, setCards] = useState([]);;
   const [currentCard, setCurrentCard] = useState(null);
   const [previousCard, setPreviousCard] = useState(null);
-  const [matchStatus, setMatchStatus] = useState(null);
   const [snapSuitCount, setSnapSuitCount] = useState(0);
   const [snapValueCount, setSnapValueCount] = useState(0);
-  const [allCardsWithdrawn, setAllCardsWithdrawn] = useState(false);
+  const [snapStatus, setSnapStatus] = useState('');
 
   useEffect(() => {
     const fetchDeck = async () => {
       try {
-        const deckResponse = await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
+        const deckResponse = await fetch('https://deckofcardsapi.com/api/deck/new/draw/?count=52');
         const fetchedDeck = await deckResponse.json();
-        setDeck(fetchedDeck);
+        setDeck(fetchedDeck.cards);
       } catch (error) {
         alert('Error fetching data from deckofcardsapi.com');
         console.log(error);
@@ -31,88 +29,76 @@ const Snap = () => {
     const checkMatchingCards = () => {
       if (currentCard.suit === previousCard.suit) {
         setSnapSuitCount(snapSuitCount => snapSuitCount + 1);
-        setMatchStatus('SNAP SUIT!');
+        setSnapStatus('SNAP SUIT!');
       } else if (currentCard.value === previousCard.value) {
         setSnapValueCount(snapValueCount => snapValueCount + 1);
-        setMatchStatus('SNAP VALUE!');
+        setSnapStatus('SNAP VALUE!');
       } else {
-        setMatchStatus('');
+        setSnapStatus('');
       }
     }
 
     if (currentCard && previousCard) {
       checkMatchingCards();
     }
-
   }, [currentCard, previousCard]);
 
-
   const drawCard = async () => {
-    const fetchedDraw = await fetchDraw();
-    const fetchedCard = fetchedDraw.cards[0];
-    const isLastCard = cards.length === 52;
-
-    if (isLastCard) setAllCardsWithdrawn(true);
-    setCurrentCard(fetchedCard);
+    const drawnCard = deck[deck.length - 1];
+    setCurrentCard(drawnCard);
     setPreviousCard(cards[cards.length - 1]);
-    setCards([...cards, fetchedCard]);
-  };
-
-  const fetchDraw = async () => {
-    try {
-      const drawResponse = await fetch(`https://deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=1`);
-      const fetchedDraw = await drawResponse.json();
-      return fetchedDraw;
-    } catch (error) {
-      alert('Error fetching data from deckofcardsapi.com');
-      console.log(error);
-    }
+    setDeck(deck.slice(0, deck.length - 1))
+    setCards([...cards, drawnCard]);
   };
 
   const calculateNextSnapValueChance = () => {
     const sameValueOnHand = cards.filter(card => card.value === currentCard.value).length;
     const remaining = 4 - sameValueOnHand;
-    return Math.round((remaining / (52 - cards.length)) * 100);
+    return Math.round((remaining / deck.length) * 100);
   };
 
   const calculateNextSnapSuitChance = () => {
     const sameSuitOnHand = cards.filter(card => card.suit === currentCard.suit).length;
     const remaining = 13 - sameSuitOnHand;
-    return Math.round((remaining / (52 - cards.length)) * 100);
+    return Math.round((remaining / deck.length) * 100);
   }
 
   return (
-    <div className='snap-container'>
-      <div className='snap-status'>
-        <h2>{matchStatus}</h2>
-        {currentCard && (
-          <div className='draw-predictions'>
-            <span>Card {cards.length} of 52</span>
-            <span>Next Snap Suit Chance: {calculateNextSnapSuitChance()}%</span>
-            <span>Next Snap Value Chance: {calculateNextSnapValueChance()}%</span>
-            <span></span>
+    <>
+      {deck && (
+        <div className='snap-container'>
+          <div className='snap-status'>
+            <h2>{snapStatus}</h2>
+            {currentCard && (
+              <div className='draw-predictions'>
+                <span>Card {cards.length} of 52</span>
+                <span>Next Snap Suit Chance: {calculateNextSnapSuitChance()}%</span>
+                <span>Next Snap Value Chance: {calculateNextSnapValueChance()}%</span>
+                <span></span>
+              </div>
+            )}
           </div>
-        )}
-      </div>
 
-      <div className='cards-container'>
-        <div className='card-frame'>
-          {previousCard && <Card image={previousCard.image} code={previousCard.code} />}
-        </div>
-        <div className='card-frame'>
-          {currentCard && <Card image={currentCard.image} code={currentCard.code} />}
-        </div>
-      </div>
+          <div className='cards-container'>
+            <div className='card-frame'>
+              {previousCard && <Card image={previousCard.image} code={previousCard.code} />}
+            </div>
+            <div className='card-frame'>
+              {currentCard && <Card image={currentCard.image} code={currentCard.code} />}
+            </div>
+          </div>
 
-      {allCardsWithdrawn ? (
-        <>
-          <h2>VALUE MATCHES: {snapValueCount}</h2>
-          <h2>SUIT MATCHES: {snapSuitCount}</h2>
-        </>
-      ) : (
-        <button onClick={debounce(drawCard, 500)} className='draw-btn'>Draw card</button>
+          {deck.length === 0 ? (
+            <>
+              <h2>VALUE MATCHES: {snapValueCount}</h2>
+              <h2>SUIT MATCHES: {snapSuitCount}</h2>
+            </>
+          ) : (
+            <button onClick={drawCard} className='draw-btn'>Draw card</button>
+          )}
+        </div>
       )}
-    </div>
+    </>
   );
 };
 
